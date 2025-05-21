b9f20d58f3f00ba3a2f6a627a990e3a131992271
Leandro G. da Cruz <LeandroCruz717@outlook.com>
2025-05-21 15:19:00 -0300

    refactor: create ContactItem component and refactor date reminder dialog and Contacts page

---
+++ Devendos/Application/Views/Components/ContactItem.razor.cs
@@ -0,0 +1,43 @@
+﻿using Devendos.Application.Models;
+using Devendos.Application.Services;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Devendos.Application.Views.Components;
+
+public partial class ContactItem(
+    IContactsService contactsService,
+    IDialogService dialogService) : ComponentBase
+{
+    [Parameter]
+    public required ContactInfo ContactInfo { get; set; }
+
+    public bool Disabled { get; set; } = false;
+    
+    private async Task RemoveContactReminderDateAsync()
+    {
+        Disabled = true;
+        StateHasChanged();
+        
+        await contactsService.RemoveContactReminderDateAsync(ContactInfo.Id);
+        ContactInfo.ReminderDate = null;
+        Disabled = false;
+        StateHasChanged();
+    }
+    
+    private async Task ShowDialogAsync()
+    {
+        var parameters = new DialogParameters<SetContactReminder> {
+            {
+                x => x.ContactInfo, 
+                ContactInfo
+            },
+            {
+                x => x.OnReminderDateSet,
+                StateHasChanged
+            }
+        };
+        
+        await dialogService.ShowAsync<SetContactReminder>(null, parameters);
+    }
+}
---
+++ Devendos/Application/Views/Components/SetContactReminder.razor.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using Devendos.Application.Models;
+using Devendos.Application.Services;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Devendos.Application.Views.Components;
+
+public partial class SetContactReminder(IContactsService contactsService) : ComponentBase
+{
+    private static readonly CultureInfo PtBrCulture = new("pt-BR");
+    
+    [CascadingParameter]
+    public required IMudDialogInstance MudDialog { get; set; }
+
+    [Parameter] 
+    public required ContactInfo ContactInfo { get; set; }
+    
+    [Parameter]
+    public Action? OnReminderDateSet { get; set; }
+    
+    private async Task SaveReminderDateAsync()
+    {
+        await contactsService.SaveContactReminderDateAsync(
+            ContactInfo.Id, 
+            ContactInfo.ReminderDate);
+        
+        MudDialog.Close(DialogResult.Ok(true));
+
+        OnReminderDateSet?.Invoke();
+    }
+
+    private void Cancel() => MudDialog.Cancel();
+}
